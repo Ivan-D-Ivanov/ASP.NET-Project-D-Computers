fc09445a5bff42c33740f2c870c5d6bde06ce380
Ivan Ivanov <black_i@abv.bg>
2021-08-23 12:59:38 +0300

    Accessories button name changed.

--- Data/DavinoComputers.Data/Seeding/SubCategorySeeder.cs
+++ Data/DavinoComputers.Data/Seeding/SubCategorySeeder.cs
@@ -15,18 +15,18 @@
                 return;
             }
 
-            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "Mother Boards", CategoryId = 5 });
-            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "CPU", CategoryId = 5 });
-            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "GPU", CategoryId = 5 });
-            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "RAM", CategoryId = 5 });
-            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "Cooler Supply", CategoryId = 5 });
-            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "HDD", CategoryId = 5 });
-            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "SSD", CategoryId = 5 });
-            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "Computer Cases", CategoryId = 5 });
-            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "Mouses", CategoryId = 6 });
-            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "Keyboards", CategoryId = 6 });
-            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "HeadSet", CategoryId = 6 });
-            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "Mouse Pads", CategoryId = 6 });
+            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "Mother Boards", CategoryId = 4 });
+            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "CPU", CategoryId = 4 });
+            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "GPU", CategoryId = 4 });
+            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "RAM", CategoryId = 4 });
+            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "Cooler Supply", CategoryId = 4 });
+            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "HDD", CategoryId = 4 });
+            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "SSD", CategoryId = 4 });
+            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "Computer Cases", CategoryId = 4 });
+            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "Mouses", CategoryId = 5 });
+            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "Keyboards", CategoryId = 5 });
+            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "HeadSet", CategoryId = 5 });
+            await dbContext.SubCategories.AddAsync(new SubCategory { Name = "Mouse Pads", CategoryId = 5 });
 
             await dbContext.SaveChangesAsync();
         }
